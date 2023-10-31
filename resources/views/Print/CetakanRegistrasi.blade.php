589c7cb833ff24e689e20633b6795c14d3eefa42
ilhamfardiansyah <ilhamfardiansyah97@gmail.com>
2023-10-31 16:33:13 +0700

    update cetakan

--- resources/views/Print/CetakanRegistrasi.blade.php
+++ resources/views/Print/CetakanRegistrasi.blade.php
@@ -43,6 +43,7 @@ position:relative;">
 
     <section class="sheet padding-10mm">
         <div style="margin:0 1rem; display: flex; justify-content: center; align-items:center">
+
             <table style="padding: 0.5rem 1rem; border:1px solid #000000">
                 <tr>
                     <td rowspan="3" style="padding: 0.5rem 1rem; border:1px solid #000000; height: 3rem;">
@@ -50,57 +51,66 @@ position:relative;">
                     </td>
                 </tr>
                 <tr>
-                    <td class="text-center" style="height: 3rem; width: 35rem">Laboratorium Digiral Forensik Kejaksaan
-                        R.I
+                    <td class="text-center" style="height: 3rem; width: 35rem">
+                        <h5>Laboratorium Digiral Forensik Kejaksaan
+                            R.I
                     </td>
                 </tr>
                 <tr style="padding: 0.5rem 1rem; border:1px solid #000000; height: 3rem;">
-                    <td class="text-center"><b>Registrasi Penerimaan Barang Bukti Elektronik</td>
+                    <td class="text-center">
+                        <h5><b>
+                                Registrasi Penerimaan Barang Bukti Elektronik
+                    </td>
                 </tr>
-                <tr style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">
-                    <td class="text-center">FM7.4-1</td>
-                    <td class="text-center">FM7.4-1</td>
+                <tr style="padding: 0.5rem 0.5rem; border:1px solid #000000; height: 0.5rem;">
+                    <td rowspan="3" style="border: 1px solid #000000;">
+                        <h6 class="text-center">FM7.4-1
+                    </td>
+                    <td colspan="3" style="border: 1px solid #000000;">
+                        <h6 class="text-center">FM7.4-1
+                    </td>
+
                 </tr>
             </table>
         </div>
 
-        <table style="margin: 25px; border:0.5px; padding: 0.5rem 0.5rem">
-            <tr style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">1</td>
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">Nama Kasus</td>
+        <table style="margin: 25px; border:0.5px;">
+            <tr style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">1</td>
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">Nama Kasus</td>
                 <td style="width: 35em; vertical-align: top;"></td>
             </tr>
-            <tr style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">2</td>
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">Jenis BBE</td>
+            <tr style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">2</td>
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">Jenis BBE</td>
                 <td style="width: 35em; vertical-align: top;"></td>
             </tr>
-            <tr style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">3</td>
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">Spesifikasi BBE</td>
+            <tr style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">3</td>
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">Spesifikasi BBE</td>
                 <td style="width: 35em; vertical-align: top;"></td>
             </tr>
-            <tr style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">4</td>
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">Pemilik BBE</td>
+            <tr style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">4</td>
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">Pemilik BBE</td>
                 <td style="width: 35em; vertical-align: top;"></td>
             </tr>
-            <tr style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">5</td>
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">IMEI/Serial Number</td>
-                <td style="width: 35em; vertical-align: top;">IMEI 1
+            <tr style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">5</td>
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">IMEI/Serial Number</td>
+                <td style="width: 35em; vertical-align: top;">IMEI 1 :
                     <br>
                     <br>
-                    IMEI 2
+                    IMEI 2 :
                     <br>
                     <br>
-                    S/N
+                    S/N :
                     <br>
                 </td>
             </tr>
-            <tr style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">6</td>
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">Parameter Security</td>
+            <tr style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">6</td>
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">Parameter Security</td>
                 <td style="width: 35em; vertical-align: top;">
                     <br>
                     <br>
@@ -109,137 +119,39 @@ position:relative;">
                     <br>
                 </td>
             </tr>
-            <tr style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">7</td>
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">Kondisi Ketika Diterima</td>
+            <tr style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">7</td>
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">Kondisi Ketika Diterima</td>
                 <td style="width: 35em; vertical-align: top;"></td>
             </tr>
-            <tr style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">9</td>
-                <td style="padding: 0.5rem 1rem; border:1px solid #000000; height: 1rem;">Waktu Diterima</td>
+            <tr style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">8</td>
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">Waktu Diterima</td>
                 <td style="width: 35em; vertical-align: top;"></td>
             </tr>
+
             <tr>
                 <td style="width: 1em; vertical-align: top; height: 2px"><b>B.</td>
                 <td style="width: 20em; vertical-align: top;"><b>Verifikasi Permohonan</td>
             </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">4.</td>
-                <td style="width: 20em; vertical-align: top;">Jabatan</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->jabatan ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">5.</td>
-                <td style="width: 20em; vertical-align: top;">Kesatuan</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->kesatuan ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">6.</td>
-                <td style="width: 20em; vertical-align: top;">No Passring</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->nopassring ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">7.</td>
-                <td style="width: 20em; vertical-align: top;">Tanda Jasa</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->tandajasa ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">8.</td>
-                <td style="width: 20em; vertical-align: top;">TGL / Umur</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->tgl ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">9.</td>
-                <td style="width: 20em; vertical-align: top;">Suku Bangsa</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->suku ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">10.</td>
-                <td style="width: 20em; vertical-align: top;">Agama</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->agama ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">11.</td>
-                <td style="width: 20em; vertical-align: top;">Tinggi / Berat</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->tinggi ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">12.</td>
-                <td style="width: 20em; vertical-align: top;">Warna Kulit</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->warnakulit ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">13.</td>
-                <td style="width: 20em; vertical-align: top;">Mata</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->mata ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">14.</td>
-                <td style="width: 20em; vertical-align: top;">Rambut</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->rambut ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">15.</td>
-                <td style="width: 20em; vertical-align: top;">Gol Darah</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->goldarah ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">16.</td>
-                <td style="width: 20em; vertical-align: top;">Alamat Sekarang</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->alamatsekarang ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">17.</td>
-                <td style="width: 20em; vertical-align: top;">Nama Ayah/Alamat</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->ayahalamat ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">18.</td>
-                <td style="width: 20em; vertical-align: top;">Nama Ibu/Alamat</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->ibualamat ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">19.</td>
-                <td style="width: 20em; vertical-align: top;">Nama Istri/Alamat</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->istrialamat ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">20.</td>
-                <td style="width: 20em; vertical-align: top;">Tempat/No Akte Nikah</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->tempatnikah ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">21.</td>
-                <td style="width: 20em; vertical-align: top;">Nama Anak Kandung</td>
-                <td style="vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->namaanak ?? '' }}</td>
-            </tr>
-            <tr>
-                <td style="width: 1em; vertical-align: top;">22.</td>
-                <td style="width: 20em; vertical-align: top;">Nama Ortu Istri/Suami</td>
-                <td style="width: 1em; vertical-align: top">:</td>
-                <td style="width: 35em; vertical-align: top;">{{ $dataPegawai->ortuistrialamat ?? '' }}</td>
-            </tr>
 
-
+            <tr style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">9</td>
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">Nama Pemohon</td>
+                <td style="width: 35em; vertical-align: top;"></td>
+            </tr>
+            <tr style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">10</td>
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">NIP/NRP</td>
+                <td style="width: 35em; vertical-align: top;"></td>
+            </tr>
+            <tr style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">11</td>
+                <td style="padding: 0.1rem 1rem; border:1px solid #000000; height: 1rem;">Jabatan</td>
+                <td style="width: 35em; vertical-align: top;"></td>
+            </tr>
         </table>
+
         <button onclick="window.print();" class="btn btn-success no-print"
             style="position: fixed; bottom:35px; right:10em;">Print</button>
         <form action="/home" method="get">
