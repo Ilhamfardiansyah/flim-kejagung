8f2c82f0522f73ea3f8e646e2accb2792fa3f070
ilhamfardiansyah <ilhamfardiansyah97@gmail.com>
2023-12-19 14:51:47 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,7 +38,7 @@ class HomeController extends Controller
 
     public function regist()
     {
-        return view('Dashboard.Regist');
+        return view('Dashboard.regist');
     }
 
     public function coc()
