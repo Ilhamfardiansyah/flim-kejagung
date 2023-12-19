e21814e84321f765201b3ee1f8649122bc1d0630
ilhamfardiansyah <ilhamfardiansyah97@gmail.com>
2023-12-19 14:57:12 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller
 
     public function coc()
     {
-        return view('Dashboard.Coc');
+        return view('Dashboard.coc');
     }
 
     public function print()
